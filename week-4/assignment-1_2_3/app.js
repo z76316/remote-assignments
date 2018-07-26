42d801c71f1b992a0c154e5cc3490c3a81246255
z76316 <z7631614@gmail.com>
2018-07-26 20:22:59 +0800

    updated the assignment 2, build simple RESTful API

--- week-4/assignment-1_2_3/app.js
+++ week-4/assignment-1_2_3/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -8,4 +9,22 @@ app.get('/', (req, res) => {
 	res.render('index');
 });
 
-app.listen(3000);
+app.get('/getData', (req, res) => {
+	const number = req.query.number;
+	// console.dir(number);
+	if (number) {
+		// total 1 + .... + number
+		var total = 0;
+		for (let i = 1; i <= number; i++)
+		{
+			total += i;
+		}
+		res.render('getData', { total });
+	} else {
+		res.render('getData');
+	}
+});
+
+app.listen(3000, () => {
+	console.log('The application is running on localhost:3000!');
+});
